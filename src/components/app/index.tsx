75dcabcd1c7872f65883b0408950ef00d96b30ac
Stanisław Markowski <stasiomimi@hotmail.com>
2022-10-30 14:02:27 +0100

    Add navbar to the page

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,9 +1,11 @@
 import "./styles.css";
 import { WorkInProgressBanner } from "../workInProgressBanner/WorkInProgressBanner";
+import { AppNavbar } from "../navbar";
 
 function App() {
   return (
     <>
+      <AppNavbar />
       <div className="main">
         <WorkInProgressBanner />
       </div>
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,29 +1,26 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import "../../globalStyles.css";
 
 export function AppNavbar() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar className="navbar-dark bg-dark" expand="lg" sticky="top">
+      <Container fluid className="mx-lg-5">
+        <Navbar.Brand className="bajer-font fs-2" href="/">
+          Bajer
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="text-uppercase d-lg-flex justify-content-end"
+        >
+          <Nav>
+            <Nav.Link>O klubie</Nav.Link>
+            <Nav.Link>Ekipa</Nav.Link>
+            <Nav.Link>Oferta</Nav.Link>
+            <Nav.Link>Wynajmy</Nav.Link>
+            <Nav.Link>Kontakt</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
