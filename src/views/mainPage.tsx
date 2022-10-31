e5c29ac6b2861e3520b5a4d058729ba970714f9d
Stanisław Markowski <stasiomimi@hotmail.com>
2022-10-31 19:06:18 +0100

    Add info about management

--- src/views/mainPage.tsx
+++ src/views/mainPage.tsx
@@ -2,12 +2,22 @@ import { AboutUsBanner } from "../components/aboutUsBanner";
 import { ManagementPhotoGalleryBanner } from "../components/managementPhotoGalleryBanner";
 import { WelcomeBanner } from "../components/welcomeBanner";
 
+const managementMembers = {
+  president: { title: "Kajetan Skolmowski", subtitle: "Prezes" },
+  otherMembers: [
+    { title: "Kajetan Skolmowski", subtitle: "Prezes" },
+    { title: "Kajetan Skolmowski", subtitle: "Prezes" },
+    { title: "Kajetan Skolmowski", subtitle: "Prezes" },
+    { title: "Kajetan Skolmowski", subtitle: "Prezes" },
+  ],
+};
+
 export function MainPage() {
   return (
     <>
       <WelcomeBanner />
       <AboutUsBanner />
-      <ManagementPhotoGalleryBanner />
+      <ManagementPhotoGalleryBanner {...managementMembers} />
     </>
   );
 }
